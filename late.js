95d33b67603d8cf73ecfad58a370753b5850445a
macbarnes <jmacbarnes04@gmail.com>
2022-05-01 16:32:40 -0400

    late function

--- late.js
+++ late.js
@@ -1,1 +1,11 @@
 // start coding your function here!
+function running_late(date) {
+    if (hours >= 22 ){
+        response = "It is late!"
+    } else {
+        response = "It is still early!"
+    }
+    return response
+} 
+
+exports.running_late = running_late;
