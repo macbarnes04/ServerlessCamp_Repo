60368115e580c178ce6a4620c268f9a650bf6f12
macbarnes <jmacbarnes04@gmail.com>
2022-05-01 17:11:38 -0400

    fix late function

--- late.js
+++ late.js
@@ -1,11 +1,11 @@
 // start coding your function here!
 function running_late(date) {
-    if (date.getHours <= 22 ){
-        return "It is still early!"
+    if (date.getHours() <= 22 ){
+        return "It is still early!";
     } 
     else {
-        return "It is late!"
+        return "It is late!";
     } 
 } 
-running_late(new Date())
-exports.running_late = running_late;
+
+console.log(running_late(new Date()));
