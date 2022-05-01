bee7d892fac7350c5c2d7e753bf602cfdf8ae0af
macbarnes <jmacbarnes04@gmail.com>
2022-05-01 17:49:03 -0400

    first try

--- max_nums.js
+++ max_nums.js
@@ -1,1 +1,11 @@
 // start coding your function here!
+function find_max(arr) {
+    var biggest = arr[0]
+    for(var i = 1; i < arr.length; i++) {
+        biggest = biggest > arr[i] ? biggest : arr[i]  
+      }
+    return biggest
+} 
+
+console.log(find_max([1, 78, 12, 2, 5, 10]))
+exports.find_max = find_max
